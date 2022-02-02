13bbb015666ad5cef50f8ae0cbdcbabafc1c139d
Neel Yadav <neel25y@outlook.com>
2022-02-02 01:36:35 +0000

    Update kyokan-bob from 0.8.0 to 0.9.0

--- Casks/kyokan-bob.rb
+++ Casks/kyokan-bob.rb
@@ -1,6 +1,6 @@
 cask "kyokan-bob" do
-  version "0.8.0"
-  sha256 "66a49484070979a3215bf164a47a33d1035795ad3d513a598cbdaa7896c7bf81"
+  version "0.9.0"
+  sha256 "fb097ae8405705de007fe86def051b259de38c4b969ffff2f3cd081b80b95eeb"
 
   url "https://github.com/kyokan/bob-wallet/releases/download/v#{version}/Bob-#{version}.dmg",
       verified: "github.com/kyokan/bob-wallet/"
