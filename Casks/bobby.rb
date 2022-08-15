383e5cd6173878d23f2bb0fac29e7e0df96e17ca
Neel Yadav <mail@neelyadav.com>
2022-08-15 02:28:24 -0500

    test auto-bumping multi-arch homebrew cask

--- Casks/bobby.rb
+++ Casks/bobby.rb
@@ -1,16 +1,22 @@
 cask "bobby" do
-  version "0.999.2"
-  sha256 "fb097ae8405705de007fe86def051b259de38c4b969ffff2f3cd081b80b95eeb"
+  version "0.999.3"
 
-  url "https://github.com/nlydv/bob-wallet/releases/download/v#{version}/Bob-#{version}.dmg",
+  if Hardware::CPU.intel?
+    arch = "x86"
+    sha256 "fb097ae8405705de007fe86def051b259de38c4b969ffff2f3cd081b80b95eeb"
+  else
+    arch = "arm64"
+    sha256 "fb097ae8405705de007fe86def051b259de38c4b969ffff2f3cd081b80b95eeb"
+  end
+
+  url "https://github.com/nlydv/bob-wallet/releases/download/v#{version}/Bob-#{version}-#{arch}.dmg",
       verified: "github.com/nlydv/bob-wallet/"
   name "Bob Wallet"
-  desc "Handshake wallet GUI for name auction and DNS record management"
+  desc "Handshake wallet application for managing transactions, domain name auctions, and DNS records"
   homepage "https://bobwallet.io/"
 
   livecheck do
     url :url
-    strategy :git
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
